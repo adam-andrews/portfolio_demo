10ab99f1e3729581ef3729d2cd06c2a60d437ff8
Adam Andrews <andrewsadam07@gmail.com>
2022-03-02 14:06:01 +0000

    Minor changes

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -42,11 +42,7 @@ const Footer = () => {
       <div className="app__footer-cards">
         <div className="app__footer-card ">
           <img src={images.email} alt="email" />
-          <a href="mailto:hello@micael.com" className="p-text">hello@micael.com</a>
-        </div>
-        <div className="app__footer-card">
-          <img src={images.mobile} alt="phone" />
-          <a href="tel:+1 (123) 456-7890" className="p-text">+1 (123) 456-7890</a>
+          <a href="mailto:adampandrews2:gmail.com" className="p-text">adampandrews2@gmail.com</a>
         </div>
       </div>
       {!isFormSubmitted ? (
